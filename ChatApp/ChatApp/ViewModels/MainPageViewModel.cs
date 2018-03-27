de8ec201edf1e284e748bea29bf311c42cefb19c
Vincent Nwonah <vnwonah@outlook.com>
2018-03-27 09:39:24 +0100

    Added User Log In View

---
+++ ChatApp/ChatApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChatApp.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
