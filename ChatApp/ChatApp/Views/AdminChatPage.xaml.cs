43f000b42d948cb0cc913523a31573e0d61c0268
Vincent Nwonah <vnwonah@outlook.com>
2018-03-28 17:32:10 +0100

    AdminChat Working

--- ChatApp/ChatApp/Views/AdminChatPage.xaml.cs
+++ ChatApp/ChatApp/Views/AdminChatPage.xaml.cs
@@ -16,6 +16,7 @@ namespace ChatApp.Views
 	{
         private string _name;
         private UserPerson _person;
+        int count = 0;
 		public AdminChatPage (string name)
 		{
 			InitializeComponent ();
@@ -24,6 +25,128 @@ namespace ChatApp.Views
 
             _person = AdminServices.userPeople.Where(p => p.Name == _name).FirstOrDefault();
 
+            sendButton.Clicked += SendButton_ClickedAsync;
+
+            ChatClientService.OnMessageRecieved += ChatClientService_OnMessageRecieved;
+
+            ReelMessages(_person.Messages);
+
+        }
+
+        private void ChatClientService_OnMessageRecieved(object sender, Message e)
+        {
+            if (e.Reciever == "admin")
+            {
+                StackLayout sL = new StackLayout();
+                sL.HorizontalOptions = LayoutOptions.End;
+                sL.Padding = 10;
+                sL.BackgroundColor = Color.FromHex("#F6F6F6");
+
+                Label lbl = new Label();
+                lbl.Text = e.Content;
+                lbl.TextColor = Color.FromHex("#2196F3");
+
+                sL.Children.Add(lbl);
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    this.parentStack.Children.Add(sL);
+                    scrollView.ScrollToAsync(parentStack.Width, parentStack.Height, true);
+
+                    messageEntry.Text = "";
+                });
+
+
+            }
+
+            else if (e.Owner == _name)
+            {
+                StackLayout sL = new StackLayout();
+                sL.HorizontalOptions = LayoutOptions.Start;
+                sL.Padding = 10;
+                sL.BackgroundColor = Color.FromHex("#2196F3");
+
+                Label lbl = new Label();
+                lbl.Text = e.Content;
+                lbl.TextColor = Color.White;
+
+                sL.Children.Add(lbl);
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    this.parentStack.Children.Add(sL);
+                    scrollView.ScrollToAsync(parentStack.Width, parentStack.Height, true);
+
+                    messageEntry.Text = "";
+                });
+
+            }
+        }
+
+        private async void SendButton_ClickedAsync(object sender, EventArgs e)
+        {
+            await ChatClientService.Send(new Message
+            {
+                Id = count,
+                Owner = _name,
+                Reciever = "admin",
+                Content = messageEntry.Text
+            });
+
+            count++;
+        }
+
+        public void ReelMessages (List<Message> messages)
+        {
+            foreach (var message in messages)
+            {
+                if (message.Reciever == "admin")
+                {
+                    StackLayout sL = new StackLayout();
+                    sL.HorizontalOptions = LayoutOptions.End;
+                    sL.Padding = 10;
+                    sL.BackgroundColor = Color.FromHex("#F6F6F6");
+
+                    Label lbl = new Label();
+                    lbl.Text = message.Content;
+                    lbl.TextColor = Color.FromHex("#2196F3");
+
+                    sL.Children.Add(lbl);
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        this.parentStack.Children.Add(sL);
+                        scrollView.ScrollToAsync(parentStack.Width, parentStack.Height, true);
+
+                        messageEntry.Text = "";
+                    });
+
+
+                }
+
+                else
+                {
+                    StackLayout sL = new StackLayout();
+                    sL.HorizontalOptions = LayoutOptions.Start;
+                    sL.Padding = 10;
+                    sL.BackgroundColor = Color.FromHex("#2196F3");
+
+                    Label lbl = new Label();
+                    lbl.Text = message.Content;
+                    lbl.TextColor = Color.White;
+
+                    sL.Children.Add(lbl);
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        this.parentStack.Children.Add(sL);
+                        scrollView.ScrollToAsync(parentStack.Width, parentStack.Height, true);
+
+                        messageEntry.Text = "";
+                    });
+
+                }
+            }
         }
 	}
 }
